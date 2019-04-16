8ff7e0c345573331c88bd28d7c07c5a26365282c
Alex <mblahnik@uwm.edu>
2019-04-16 16:39:38 -0500

    b

--- Main/models.py
+++ Main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Account(models.Model):
     userName = models.CharField(max_length=20, default=" ")
     firstName = models.CharField(max_length=20, default=" ")
