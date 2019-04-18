6ea1361124912cbeb13ab9f91f3bf9f5a2e7a571
Erik <spykim2003@gmail.com>
2019-04-18 01:51:18 -0500

    delete account html

--- TaScheduleizer/urls.py
+++ TaScheduleizer/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('instructor/', views.instructorPage.as_view()),
     path('ta/', views.taPage.as_view()),
     path('createaccount/', views.createAccountView.as_view()),
-    path('courseassignments/', views.courseAssignmentsList.as_view())
+    path('courseassignments/', views.courseAssignmentsList.as_view()),
+    path('deleteaccount/', views.courseAssignmentsList.as_view())
 ]
