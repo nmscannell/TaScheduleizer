7ed3e5ad6dba55153d00c0ad2088f796bb56e0a0
Leuenberger <leuenbe4@uwm.edu>
2019-04-19 15:57:26 -0500

    tests for create account

--- Tests/UnitTests/test_createAccount.py
+++ Tests/UnitTests/test_createAccount.py
@@ -11,19 +11,19 @@ class TestCreateAccount(TestCase):
         self.c = Client()
         # Set up for createAccount Testing
         Account.objects.create(userName="janewayk123", firstName="Kathryn", lastName="Janeway", password="123456",
-                               email="janewayk@starfleet.com", title=2,
+                               email="janewayk@uwm.com", title=2,
                                address="14 Voyager Drive", city="Delta", state="Quadrant", zipCode="00000",
                                officeNumber="456", officePhone="555-555-5555", officeDays="TR",
                                officeHoursStart="1300", officeHoursEnd="1400", currentUser=False)
 
         Account.objects.create(userName="picard304", firstName="Jean Luc", lastName="Picard", password="90456",
-                               email="picardj@startfleet.com", title=1, address="87 Enterprise Avenue",
+                               email="picardj@uwm.com", title=1, address="87 Enterprise Avenue",
                                city="Alpha", state="Quadrant", zipCode="11111", officeNumber="54",
                                officePhone="777-777-7777", officeDays="W", officeHoursStart="0900",
                                officeHoursEnd="1000", currentUser=False)
 
         Account.objects.create(userName="kirkj22", firstName="James", lastName="Kirk", password="678543",
-                               email="kirkj22@starfleet.com", title=4, address="789 Enterprise Avenue",
+                               email="kirkj22@uwm.com", title=4, address="789 Enterprise Avenue",
                                city="Alpha", state="Quadrant", zipCode="89765", officeNumber="987",
                                officePhone="897-654-398", officeDays="MW", officeHoursStart="1500",
                                officeHoursEnd="1600", currentUser=False)
@@ -59,12 +59,30 @@ class TestCreateAccount(TestCase):
         self.assertEqual(C.email, "tuckert90@uwm.edu")
         self.assertEqual(C.title, 1)
 
+    def test_success_post(self):
+        response = self.c.post('/createaccount/', {'firstname': 'Wesley', 'lastname': 'Crusher',
+                                                   'username': 'crusher31', 'title': 'TA',
+                                                   'email': 'crusher31@uwm.edu'})
+        self.assertEqual(response.context['message'],
+                         "Account successfully created.  Temporary password is: crusher31456")
+        A = Account.objects.get(userName="crusher31")
+        self.assertEqual(A.firstName, "Wesley")
+        self.assertEqual(A.lastName, "Crusher")
+        self.assertEqual(A.email, "crusher31@uwm.edu")
+        self.assertEqual(A.title, 1)
+
 
     def test_account_already_exist(self):
         self.assertEqual(Commands.createAccount("Jean Luc", "Picard", "picard304", "TA", "picardj@uwm.edu"),
                          "Account already exists")
         self.assertEqual(Commands.createAccount( "Kathryn", "Janeway", "janewayk123", "instructor", "janewayk@uwm.edu"),
                          "Account already exists")
+
+    def test_account_already_exists_post(self):
+        response = self.c.post('/createaccount/', {'firstname': 'Kathryn', 'lastname': 'Janeway',
+                                                   'username': 'janewayk123', 'title': 'Instructor',
+                                                   'email':'janewayk123@uwm.edu'})
+        self.assertEqual(response.context['message'], "Account already exists")
 
     def test_invalid_email(self):
         self.assertEqual(Commands.createAccount("Wesley", "Crusher", "crusherw31", "TA", "crusher@hotmail.com"),
@@ -74,15 +92,18 @@ class TestCreateAccount(TestCase):
                          "The email address you have entered in not valid.  Please make sure you are using a uwm "
                          "email address in the correct format.")
 
+    def test_invalid_email_post(self):
+        response = self.c.post('/createaccount/', {'firstname': 'Beverly', 'lastname': 'Crusher',
+                                        'username': 'bcrusher', 'title': 'Instructor',
+                                        'email': 'bcrusher@starfleet.edu'})
+        self.assertEqual(response.context['message'], "The email address you have entered in not valid.  "
+                                            "Please make sure you are using a uwm email address in the correct format.")
+
     def test_invalid_title(self):
         self.assertEqual(Commands.createAccount("Wesley", "Crusher", "crusher31", "student", "crusher31@uwm.edu"),
                          "Invalid title, account not created")
 
-    def test_something(self):
-        self.c.post('/createaccount/', {'firstname':'Wesley', 'lastname':'Crusher',
-                                              'username': 'crusher31', 'title':'TA', 'email':'crusher31@uwm.edu'})
-        A = Account.objects.get(userName="crusher31")
-        self.assertEqual(A.firstName, "Wesley")
-        self.assertEqual(A.lastName, "Crusher")
-        self.assertEqual(A.email, "crusher31@uwm.edu")
-        self.assertEqual(A.title, 1)
+    def test_invalid_title_post(self):
+        response = self.c.post('/createaccount/', {'firstname': 'Beverly', 'lastname': 'Crusher',
+                                                   'username': 'bcrusher', 'title': 'Doctor',
+                                                   'email': 'bcrusher@uwm.edu'})
