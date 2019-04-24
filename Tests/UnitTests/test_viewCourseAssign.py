65a900babd56494e4f40eaf0fea36bcb600266f8
mblahnik <mblahnik@uwm.edu>
2019-04-24 18:21:21 -0500

    Create course acceptance tests work!

--- Tests/UnitTests/test_viewCourseAssign.py
+++ Tests/UnitTests/test_viewCourseAssign.py
@@ -6,10 +6,10 @@ from Main.models import Account, Course, Section, AccountSection, AccountCourse
 class TestViewCourseAssign(TestCase):
 
     def setUp(self):
-            self.account1 = Account.objects.create(userName='instructor', title='2')
-            self.account2 = Account.objects.create(userName='taman', title='1')
-            self.account3 = Account.objects.create(userName='joe', title='1')
-            self.account4 = Account.objects.create(userName='admin', title='3')
+            self.account1 = Account.objects.create(userName='instructor', title='2', firstName="Chip", lastName="Whitley")
+            self.account2 = Account.objects.create(userName='taman', title='1', firstName="Rob", lastName="Buzzsaw")
+            self.account3 = Account.objects.create(userName='joe', title='1', firstName="Joe", lastName="Cool")
+            self.account4 = Account.objects.create(userName='admin', title='3', firstName="Jedediah", lastName="Longtree")
             self.course1 = Course.objects.create(number='361', name='IntroSoftwareEngineering')
             self.course2 = Course.objects.create(number='101', name='English')
             self.course3 = Course.objects.create(name='History', number='101')
@@ -28,22 +28,22 @@ class TestViewCourseAssign(TestCase):
     def test_viewCourseAssign_Instructor_1Course(self):
         AccountCourse.objects.get(Account=self.account1, Course=self.course2).delete()
 
-        self.assertEqual(viewCourseAssign("instructor"), "instructor is assigned to: IntroSoftwareEngineering")
+        self.assertEqual(viewCourseAssign("instructor"), "Chip Whitley is assigned to: IntroSoftwareEngineering")
 
     def test_viewCourseAssign_Instructor_2Course(self):
 
-        self.assertEqual(viewCourseAssign("instructor"), "instructor is assigned to: IntroSoftwareEngineering, English")
+        self.assertEqual(viewCourseAssign("instructor"), "Chip Whitley is assigned to: IntroSoftwareEngineering, English")
 
     def test_viewCourseAssign_TA_CourseLab(self):
 
-        self.assertEqual(viewCourseAssign("taman"), "taman is assigned to: IntroSoftwareEngineering section 801")
+        self.assertEqual(viewCourseAssign("taman"), "Rob Buzzsaw is assigned to: LAB 801")
 
     def test_viewCourseAssign_Ta_2CourseLab(self):
         self.lab2 = Section.objects.create(number='801', course=self.course2)
         AccountSection.objects.create(Account=self.account2, Section=self.lab2)
 
         self.assertEqual(viewCourseAssign("taman"),
-                         "taman is assigned to: IntroSoftwareEngineering section 801, English section 801")
+                         "Rob Buzzsaw is assigned to: IntroSoftwareEngineering section 801, English section 801")
 
     def test_viewCourseAssign_Ta_2CourseLab_1Course(self):
         self.lab2 = Section.objects.create(number='801', course=self.course2)
@@ -56,9 +56,9 @@ class TestViewCourseAssign(TestCase):
     def test_viewCourseAssign_TA_noLabs(self):
         AccountSection.objects.get(Account=self.account2).delete()
 
-        self.assertEqual(viewCourseAssign("taman"), "taman is assigned to: IntroSoftwareEngineering")
+        self.assertEqual(viewCourseAssign("taman"), "Rob Buzzsaw is assigned to: IntroSoftwareEngineering")
 
     def test_viewCourseAssign_Ta_1CourseLab_1Course(self):
         AccountCourse.objects.create(Account=self.account2, Course=self.course2)
 
-        self.assertEqual(viewCourseAssign("taman"), "taman is assigned to: IntroSoftwareEngineering section 801, English")
+        self.assertEqual(viewCourseAssign("taman"), "Rob Buzzsaw is assigned to: LAB 801, English")
