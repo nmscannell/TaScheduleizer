766aa86fe2a3022ede642bcd500e5940a9bfae84
Leuenberger <leuenbe4@uwm.edu>
2019-05-09 15:06:11 -0500

    fixed del account tests

--- Tests/AcceptanceTests/test_web.py
+++ Tests/AcceptanceTests/test_web.py
@@ -1206,11 +1206,11 @@ class Test_web(TestCase):
 
     def test_deleteCourse_Success(self):
         self.c.post('/login/', {'username': 'kirkj22', 'password': '678543'})
-        response = self.c.post('/deletecourse/', {'name': 'TemporalMechanics'})
+        response = self.c.post('/deletecourse/', {'coursename': 'TemporalMechanics'})
         self.assertEqual(response.context['message'], "Course successfully deleted")
 
     def test_deleteCourse_notFound(self):
         self.c.post('/login/', {'username': 'kirkj22', 'password': '678543'})
-        response = self.c.post('/deletecourse/', {'name': 'secretCourse'})
-        self.assertEqual(response.context['message'], "Course not found")
+        response = self.c.post('/deletecourse/', {'coursename': 'secretCourse'})
+        self.assertEqual(response.context['message'], "Course does not exist")
 
